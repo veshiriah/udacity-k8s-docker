440de92484a12f8bb8277175b1acf25df803d919
Kinga Fil <kingafil@deloitte.com.au>
2021-01-09 17:14:13 +1100

    uploading upload docker and updating circleci config to match the yml

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=kchachowska/udacity-microservices
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+cat ~/Desktop/udacity/microservices/final_project/password.txt | docker login --username kchachowska --password-stdin
+docker tag udacity-microservices $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath

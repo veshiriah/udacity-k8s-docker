6f1cafa44a367d4334e13a39f25b5912f8ffd687
Kinga Fil <kingafil@deloitte.com.au>
2021-01-09 18:11:05 +1100

    changes to kubernetes to use a general name for a pod

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -17,4 +17,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward udacity-microservices-7854ff557c-krqgq 8000:80
+kubectl port-forward deployment/udacity-microservices 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -11,7 +11,9 @@ dockerpath=kchachowska/udacity-microservices
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+
 cat ~/Desktop/udacity/microservices/final_project/password.txt | docker login --username kchachowska --password-stdin
+
 docker tag udacity-microservices $dockerpath
 # Step 3:
 # Push image to a docker repository

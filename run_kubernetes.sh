23814f9b34e8d0512313f7e893eac54806a78dc5
Kinga Fil <kingafil@deloitte.com.au>
2021-01-09 16:58:27 +1100

    fixing make lint issues - adding requirements (adding pylint) fix app.py, adding docker upload and kubernetes run files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=kchachowska/udacity-microservices
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-microservices --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-microservices-7854ff557c-krqgq 8000:80
